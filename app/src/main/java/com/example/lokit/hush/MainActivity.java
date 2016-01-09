35d799840d1b8e4a20ec07d3d2f44fc5fd4606b6
Lokit Kumar Paras <lokit95@gmail.com>
2016-01-09 23:35:31 +0530

    Maps working

--- app/src/main/java/com/example/lokit/hush/MainActivity.java
+++ app/src/main/java/com/example/lokit/hush/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(MainActivity.this,Home.class);
+                Intent mainIntent = new Intent(MainActivity.this,MapsActivity.class);
                 MainActivity.this.startActivity(mainIntent);
                 MainActivity.this.finish();
             }
